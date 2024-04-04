4555a2df9aab04f58fe55e6ec466187e0a1f6784
CameronMooreGit <119544985+CameronMooreGit@users.noreply.github.com>
2024-04-04 01:17:13 -0400

    Update AdjacencyList.h

--- src/AdjacencyList.h
+++ src/AdjacencyList.h
@@ -1,15 +1,28 @@
-#pragma once
+#ifndef PAGERANK_ADJACENCYLIST_H
+#define PAGERANK_ADJACENCYLIST_H
 
+
+#include <iostream>
 #include <string>
-
+#include <vector>
+#include <map>
+#include <algorithm>
 using namespace std;
 
 class AdjacencyList {
-    private:
-    //Think about what member variables you need to initialize
-    public:
-    //Think about what helper functions you will need in the algorithm
-    string PageRank(int n);
+private:
+    //map<from, vector<pair<to, out_degree>>>
+    map<string, vector<pair<string,float>>> adjacencyList;
+    // map<URL, rank>
+    map<string, float> ranks;
+    map<string, float> oldRanks;
+public:
+    void insertVector(string from, string to);  // Inserts into adjacencyList, and out degree = 1.0
+    void initializeGraph();  // Assigns proper out degrees and assigns ranks to r(0)
+    void powerIterate(int n);  // Power iterates n times
+    void printPageRank();
 };
+// prints the PageRank of all pages after p powerIterations in ascending alphabetical order of webpages and rounding rank to two decimal places
 
-// This class and method are optional.
+
+#endif //PAGERANK_ADJACENCYLIST_H
