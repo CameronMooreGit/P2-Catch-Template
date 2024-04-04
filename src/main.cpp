7bd0bd0147e2a54263570093f847abc9aebd1aca
CameronMooreGit <119544985+CameronMooreGit@users.noreply.github.com>
2024-04-04 01:17:47 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
-
+#include <string>
 #include "AdjacencyList.h"
 
 using namespace std;
 
-int main() {
+int main()
+{
     int no_of_lines, power_iterations;
-    string from, to;
-    cin >> no_of_lines;
-    cin >> power_iterations;
-    for (int i = 0; i < no_of_lines; i++) {
-        cin >> from;
-        cin >> to;
-        // Do Something
+    std::string from, to;
+    AdjacencyList adjacencyList;
+    std::cin >> no_of_lines;
+    std::cin >> power_iterations;
+    for(int i = 0; i < no_of_lines; i++)
+    {
+        std::cin >> from;
+        std::cin >> to;
+        adjacencyList.insertVector(from, to);
     }
-    //Create a graph object
-    // Created_Graph.PageRank(power_iterations);}
+    adjacencyList.initializeGraph(); // Assigns out degrees and inital ranks
+    adjacencyList.powerIterate(power_iterations);  // Power iterates
+    adjacencyList.printPageRank();
 }
